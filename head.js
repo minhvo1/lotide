d921c6a255806f472c48ee3a0cb4a98d66221b67
minhvo1 <minhvo199@gmail.com>
2022-05-09 16:00:38 +0000

    refractored assertEqual and head; added test folder

--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function (array) {
   if (array.length === 0) {
     return undefined;
@@ -15,5 +7,4 @@ const head = function (array) {
   }
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
